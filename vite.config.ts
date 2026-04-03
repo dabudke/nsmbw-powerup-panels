ec884c71db2ad5ac7bb20952a5d4f2200a7f3f52
Daniel Budke <dbudke1010@outlook.com>
2026-04-03 14:48:08 -0400

    unfortunately, the plug n play experiment has failed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwind from "@tailwindcss/vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
 	base: "/nsmbw-powerup-panels/",
-	plugins: [react(), tailwind()]
+	plugins: [react(), tailwind(), ViteImageOptimizer()],
+	resolve: {
+		preserveSymlinks: true
+
+	}
 });
